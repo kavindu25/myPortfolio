e169f93316aae992cb93c22fb27733d5eb0aeae8
kavindu25 <62609657+kavindu25@users.noreply.github.com>
2021-05-30 23:30:06 +0530

    fixes

--- webapp/pages/_app.js
+++ webapp/pages/_app.js
@@ -18,27 +18,26 @@ function MyApp({ Component, pageProps }) {
         <script src="https://use.fontawesome.com/releases/v5.15.3/js/all.js" crossorigin="anonymous"></script>
         <link href="https://fonts.googleapis.com/css?family=Saira+Extra+Condensed:500,700" rel="stylesheet" type="text/css" />
         <link href="https://fonts.googleapis.com/css?family=Muli:400,400i,800,800i" rel="stylesheet" type="text/css" />
-        <link href="css/styles.css" rel="stylesheet" />
         
     </Head>
-        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav">
-            <Link href="about"><a class="navbar-brand js-scroll-trigger" >
-                <span class="d-block d-lg-none">Clarence Taylor</span>
-                <span class="d-none d-lg-block"><img class="img-fluid img-profile rounded-circle mx-auto mb-2" src="/profilepic2.jpg" alt="" /></span>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav">
+            <Link href="about"><a className="navbar-brand js-scroll-trigger" >
+                <span className="d-block d-lg-none">Clarence Taylor</span>
+                <span className="d-none d-lg-block"><img className="img-fluid img-profile rounded-circle mx-auto mb-2" src="/profilepic2.jpg" alt="" /></span>
             </a></Link>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav">
-                    <li class="nav-item"><Link  href="/about"><a class="nav-link js-scroll-trigger">About</a></Link></li>
-                    <li class="nav-item"><Link href="/experience"><a class="nav-link js-scroll-trigger">Projects</a></Link></li>
-                    <li class="nav-item"><Link href="/education"><a class="nav-link js-scroll-trigger">Education</a></Link></li>
-                    <li class="nav-item"><Link href="/skills"><a class="nav-link js-scroll-trigger">Skills</a></Link></li>
-                    <li class="nav-item"><Link href="/interests"><a class="nav-link js-scroll-trigger">Interests</a></Link></li>
-                    <li class="nav-item"><Link href="/awards"><a class="nav-link js-scroll-trigger">Awards</a></Link></li>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span></button>
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav">
+                    <li className="nav-item"><Link  href="/about"><a className="nav-link js-scroll-trigger">About</a></Link></li>
+                    <li className="nav-item"><Link href="/experience"><a className="nav-link js-scroll-trigger">Projects</a></Link></li>
+                    <li className="nav-item"><Link href="/education"><a className="nav-link js-scroll-trigger">Education</a></Link></li>
+                    <li className="nav-item"><Link href="/skills"><a className="nav-link js-scroll-trigger">Skills</a></Link></li>
+                    <li className="nav-item"><Link href="/interests"><a className="nav-link js-scroll-trigger">Interests</a></Link></li>
+                    <li className="nav-item"><Link href="/awards"><a className="nav-link js-scroll-trigger">Awards</a></Link></li>
                 </ul>
             </div>
         </nav>
-        <div class="container-fluid p-0">
+        <div className="container-fluid p-0">
         <Component {...pageProps} />
         </div>
     </>
@@ -52,10 +51,5 @@ MyApp.getInitialProps = async (appContext) => {
    return { ...appProps }
 }
     
-
-    
-
-
-
 
 export default MyApp
--- webapp/pages/skills.js
+++ webapp/pages/skills.js
@@ -1,38 +1,38 @@
 export default function FirstPost() {
-    return <section class="resume-section" id="skills">
-                <div class="resume-section-content">
-                    <h2 class="mb-5">Skills</h2>
-                    <div class="subheading mb-3">Programming Languages & Tools</div>
-                    <ul class="list-inline dev-icons">
-                        <li class="list-inline-item"><i class="fab fa-html5"></i></li>
-                        <li class="list-inline-item"><i class="fab fa-css3-alt"></i></li>
-                        <li class="list-inline-item"><i class="fab fa-js-square"></i></li>
-                        <li class="list-inline-item"><i class="fab fa-angular"></i></li>
-                        <li class="list-inline-item"><i class="fab fa-react"></i></li>
-                        <li class="list-inline-item"><i class="fab fa-node-js"></i></li>
-                        <li class="list-inline-item"><i class="fab fa-python"></i></li>
-                        <li class="list-inline-item"><i class="fab fa-java"></i></li>
-                        <li class="list-inline-item"><i class="fab fa-wordpress"></i></li>
-                        <li class="list-inline-item"><i class="fab fa-git"></i></li>
-                        <li class="list-inline-item"><i class="fab fa-docker"></i></li>
-                        <li class="list-inline-item"><i class="fab fa-npm"></i></li>
+    return <section className="resume-section" id="skills">
+                <div className="resume-section-content">
+                    <h2 className="mb-5">Skills</h2>
+                    <div className="subheading mb-3">Programming Languages & Tools</div>
+                    <ul className="list-inline dev-icons">
+                        <li className="list-inline-item"><i className="fab fa-html5"></i></li>
+                        <li className="list-inline-item"><i className="fab fa-css3-alt"></i></li>
+                        <li className="list-inline-item"><i className="fab fa-js-square"></i></li>
+                        <li className="list-inline-item"><i className="fab fa-angular"></i></li>
+                        <li className="list-inline-item"><i className="fab fa-react"></i></li>
+                        <li className="list-inline-item"><i className="fab fa-node-js"></i></li>
+                        <li className="list-inline-item"><i className="fab fa-python"></i></li>
+                        <li className="list-inline-item"><i className="fab fa-java"></i></li>
+                        <li className="list-inline-item"><i className="fab fa-wordpress"></i></li>
+                        <li className="list-inline-item"><i className="fab fa-git"></i></li>
+                        <li className="list-inline-item"><i className="fab fa-docker"></i></li>
+                        <li className="list-inline-item"><i className="fab fa-npm"></i></li>
                     </ul>
-                    <div class="subheading mb-3">Soft Skills</div>
-                    <ul class="fa-ul mb-0">
+                    <div className="subheading mb-3">Soft Skills</div>
+                    <ul className="fa-ul mb-0">
                         <li>
-                            <span class="fa-li"><i class="fas fa-check"></i></span>
+                            <span className="fa-li"><i className="fas fa-check"></i></span>
                             Communication Skills
                         </li>
                         <li>
-                            <span class="fa-li"><i class="fas fa-check"></i></span>
+                            <span className="fa-li"><i className="fas fa-check"></i></span>
                             Team work
                         </li>
                         <li>
-                            <span class="fa-li"><i class="fas fa-check"></i></span>
+                            <span className="fa-li"><i className="fas fa-check"></i></span>
                             Work Ethic
                         </li>
                         <li>
-                            <span class="fa-li"><i class="fas fa-check"></i></span>
+                            <span className="fa-li"><i className="fas fa-check"></i></span>
                             Problem Solving
                         </li>
                     </ul>
